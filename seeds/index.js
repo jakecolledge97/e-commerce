290c232709fad2fdb1e4d8c1ed6d142ef2676a64
John Conway <88188872+jakecolledge97@users.noreply.github.com>
2021-12-02 22:08:11 +1030

    changed seed file

--- seeds/index.js
+++ seeds/index.js
@@ -6,6 +6,7 @@ const seedProductTags = require('./product-tag-seeds');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
+  //drops everything before creating everything
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
   await seedCategories();
