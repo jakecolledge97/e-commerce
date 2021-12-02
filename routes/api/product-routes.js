017f3b1d6ef762e45a395864662f4162c49aed13
John Conway <88188872+jakecolledge97@users.noreply.github.com>
2021-12-02 18:14:43 +1030

    Got product .get working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try{
-    const productData = await Product.findAll();
+    const productData = await Product.findAll({include: [{model: Category}, {model: Tag}]});
     res.status(200).json(productData)
   }catch (err) {
     res.status(500).json(err)
